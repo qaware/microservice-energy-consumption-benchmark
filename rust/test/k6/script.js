8d11109766a5ecc5c8594c2b958ec36726830626
Andreas Weber <andreas.weber@qaware.de>
2023-12-13 22:40:50 +0100

    Unify docker compose setup (same container names for all tech stacks). This allows a better comparison in the green metrics tool

--- rust/test/k6/script.js
+++ rust/test/k6/script.js
@@ -18,7 +18,7 @@ export const options = {
   },
 };
 
-const baseUrl = 'http://rust:8080/api/sample/items'
+const baseUrl = 'http://app:8080/api/sample/items'
 
 const tokens = new SharedArray('tokens', function () {
   return [
