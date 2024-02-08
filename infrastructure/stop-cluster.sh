01398de5bf96477511e465d7c3419584d2d15f1b
Florian Wende <florian.wende@qaware.de>
2024-02-08 10:58:43 +0100

    Fix stop script
    * will remove loadbalancers before cluster deletion
    * will also remove EBS volumes created dynamically

--- infrastructure/stop-cluster.sh
+++ infrastructure/stop-cluster.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+
+# This will also remove the ingresses and thus the load balancers which used to block the deletion of the cluster.
+sh infrastructure/remove-exposed-services-for-green-metrics-tool.sh
+
+# Actual deletion of the cluster
 eksctl delete cluster --region=eu-north-1 --name=green-eks-k8s
 aws cloudformation delete-stack --region eu-north-1 --stack-name eksctl-green-eks-k8s-cluster
+
+# Because EBS volumes are provisioned dynamically by the EKS-EBS-Provisioner, they are not included in the CloudFormation stack and must be deleted manually
+sh infrastructure/remove-ebs-volumes.sh
