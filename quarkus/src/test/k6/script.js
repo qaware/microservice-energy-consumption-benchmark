7c3fe66812399f7294f22be2ba20cb68fb15728e
Andreas Weber <andreas.weber@qaware.de>
2024-02-12 10:55:10 +0100

    Use template for quarkus gmt measurement

--- quarkus/src/test/k6/script.js
+++ quarkus/src/test/k6/script.js
@@ -5,12 +5,12 @@ import http from 'k6/http';
 export const options = {
   scenarios: {
     overview: {
-      executor: 'constant-arrival-rate', duration: '20s', rate: 50, timeUnit: '1s',
+      executor: 'constant-arrival-rate', duration: '300s', rate: 100, timeUnit: '1s',
       preAllocatedVUs: 100, maxVUs: 300,
       exec: 'overview',
     },
     detail: {
-      executor: 'constant-arrival-rate', duration: '20s', rate: 200, timeUnit: '1s',
+      executor: 'constant-arrival-rate', duration: '300s', rate: 400, timeUnit: '1s',
       preAllocatedVUs: 100, maxVUs: 1000,
       exec: 'detail',
     },
