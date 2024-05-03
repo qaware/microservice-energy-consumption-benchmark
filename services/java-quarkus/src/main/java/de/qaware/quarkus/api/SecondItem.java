fa3622848fbed40263793852c6b41283b85dd0b8
Sascha Boehme <sascha.boehme@qaware.de>
2024-05-03 09:59:23 +0200

    Tuned code for native compilation

--- services/java-quarkus/src/main/java/de/qaware/quarkus/api/SecondItem.java
+++ services/java-quarkus/src/main/java/de/qaware/quarkus/api/SecondItem.java
@@ -1,10 +1,12 @@
 package de.qaware.quarkus.api;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Builder;
 
 import java.time.OffsetDateTime;
 
 @Builder
+@RegisterForReflection
 public record SecondItem(
     String name,
     String details,
