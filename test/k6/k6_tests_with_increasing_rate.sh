748da7b8ab89c8f044079dcf90db8707dd1e8f7b
Florian Wende <extern.wende_florian@allianz.de>
2024-05-23 13:25:42 +0200

    Reset test script to what was actually tested (tests with 10min never worked)

--- test/k6/k6_tests_with_increasing_rate.sh
+++ test/k6/k6_tests_with_increasing_rate.sh
@@ -1,12 +1,16 @@
-#echo "Warmup"
-#k6 run script.js
-#sleep 30
+#!/bin/bash
+
+echo "Running warmup..."
+k6 run script.js
+
+echo "Pausing for 30 seconds..."
+sleep 30
 
 # Loop to run the k6 script with different RATE values
 for number in {1..5}
 do
-  echo "Running k6 script with RATE=$number"
-  k6 run -e RATE=$number -e DURATION='600s' script.js
+  echo "Running k6 script with RATE=$number for 180 seconds..."
+  k6 run -e RATE=$number -e DURATION='180s' script.js
 
   # Check if this is not the last iteration
   if [ $number -ne 5 ]; then
